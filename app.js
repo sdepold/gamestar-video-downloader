4ef4f29ab9c6d6063bd1b1f79ee3acd9b6bca70b
Sascha Depold <sascha@depold.com>
2012-09-26 22:36:39 +0200

    use the video title for the filename

--- app.js
+++ app.js
@@ -11,34 +11,8 @@ helpers.crawl('http://www.gamestar.de/videos/latest/', function(html) {
     videoPage = "http://www.gamestar.de" + videoPage
 
     console.log('Processing: ' + videoPage)
-
-    helpers.crawl(videoPage, function(html) {
-      var lines = html.split('\n')
-
-      lines = lines.filter(function(line) {
-        return line.indexOf("'file':") > -1
-      }).map(function(line) {
-        return decodeURIComponent(line.split(':')[1].split(',')[0].match(/'(.*)'/)[1])
-      })
-
-      if (lines.length === 0) {
-        console.log('No results for ' + videoPage)
-      } else {
-        var path = "http://www.gamestar.de" + lines[0]
-
-        console.log('Found video on ' + videoPage + ': ' + path)
-
-        helpers.crawl(path, function(html) {
-          var $            = cheerio.load(html)
-            , downloadPath = $('body a').attr('HREF')
-
-          console.log('Start download of: ' + downloadPath)
-
-          helpers.download(downloadPath, function() {
-            console.log('Just downloaded: ' + downloadPath)
-          })
-        })
-      }
+    helpers.downloadVideo(videoPage, function() {
+      console.log('Finished: ' + videoPage)
     })
   })
 })
--- helpers.js
+++ helpers.js
@@ -1,7 +1,9 @@
-var http = require('http')
-  , exec = require('child_process').exec
+var http    = require('http')
+  , exec    = require('child_process').exec
+  , fs      = require('fs')
+  , cheerio = require('cheerio')
 
-module.exports = {
+var helpers = module.exports = {
   crawl: function(page, callback) {
     http.get(page, function(res) {
       var body = ""
@@ -16,12 +18,58 @@ module.exports = {
     })
   },
 
-  download: function(page, callback) {
-    var filename = page.split("").filter(function(char) { return !!char.match(/\w/) }).join('')
-      , cmd      = 'curl -L "' + page + '" > ' + filename + '.mp4'
+  download: function(page, title, callback) {
+    var filename = title.replace(/ /g, '-').split("").filter(function(char) {
+          return !!char.match(/[-\w]/)
+        }).join('')
+      , path     = "videos/" + filename + ".mp4"
+      , cmd      = 'curl -L "' + page + '" > ' + path
 
-    console.log('Executing: ' + cmd)
+    fs.exists(__dirname + '/' + path, function(exists) {
+      if (!exists) {
+        exec('mkdir ./videos; ' + cmd, callback)
+      } else {
+        callback()
+      }
+    })
+  },
 
-    exec(cmd, callback)
+  downloadVideo: function(pageUrl, callback) {
+    helpers.crawl(pageUrl, function(html) {
+      var lines = html.split('\n')
+        , video = null
+        , title = null
+
+      video = lines.filter(function(line) {
+        return line.indexOf("'file':") > -1
+      }).map(function(line) {
+        return decodeURIComponent(line.split(':')[1].split(',')[0].match(/'(.*)'/)[1])
+      })[0]
+
+      title = lines.filter(function(line) {
+        return line.indexOf('<title>') > -1
+      }).map(function(line) {
+        return line.match(/<title>(.*)<\/title>/)[1]
+      })[0]
+
+      if (!video) {
+        console.log('No results for ' + pageUrl)
+      } else {
+        var path = "http://www.gamestar.de" + video
+
+        title = title.replace(' - Video bei GameStar.de', '')
+
+        helpers.crawl(path, function(html) {
+          var $            = cheerio.load(html)
+            , downloadPath = $('body a').attr('HREF')
+
+          console.log('Downloading: ' + title)
+          helpers.download(downloadPath, title, function() {
+            console.log('Downloaded: ' + title)
+            callback()
+          })
+        })
+      }
+    })
   }
 }
